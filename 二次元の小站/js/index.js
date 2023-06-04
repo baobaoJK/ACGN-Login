d5c6cdf3f16e19da1a903f6225d19386db693235
baobao_JK <baobao_jk@qq.com>
2023-06-05 00:49:05 +0800

    upload

---
+++ 二次元の小站/js/index.js
@@ -0,0 +1,24 @@
+// 添加按钮事件
+document.getElementById("login-button").addEventListener("click", function () {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "") {
+        alert("请输入用户名");
+    }
+    else if (password == "") {
+        alert("请输入密码");
+    }
+    else if (username != "admin" && password != "123456") {
+        alert("用户名或密码错误");
+    }
+    else {
+        alert("登录成功");
+    }
+});
+
+// 添加随机背景事件
+var imageName = ["三叶", "泷", "三叶2", "泷2", "天气之子", "铃芽之旅"];
+var num = parseInt(Math.random() * imageName.length);
+document.getElementById("background").setAttribute("src", "images/" + imageName[num] + ".png");
+document.getElementById("background").setAttribute("alt", imageName[num] + ".png");
